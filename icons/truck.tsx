41fa896a858b89202a397c1e59c1db3ac8ebce96
Aniket-508 <pawaraniket508@gmail.com.com>
2026-01-05 16:39:49 +0530

    refactor: add speed line animations to TruckIcon

--- icons/truck.tsx
+++ icons/truck.tsx
@@ -29,6 +29,26 @@ const TRUCK_VARIANTS: Variants = {
   },
 };
 
+const SPEED_LINE_VARIANTS: Variants = {
+  normal: {
+    opacity: 0,
+    x: 0,
+    scaleX: 0,
+  },
+  animate: (custom: number) => ({
+    opacity: [0, 0.7, 0.5, 0],
+    x: [0, -4, -10, -16],
+    scaleX: [0.2, 1, 0.8, 0.3],
+    transition: {
+      duration: 0.5,
+      ease: "easeOut",
+      repeat: Number.POSITIVE_INFINITY,
+      delay: custom * 0.08,
+      times: [0, 0.2, 0.6, 1],
+    },
+  }),
+};
+
 const TruckIcon = forwardRef<TruckIconHandle, TruckIconProps>(
   ({ onMouseEnter, onMouseLeave, className, size = 28, ...props }, ref) => {
     const controls = useAnimation();
@@ -73,6 +93,7 @@ const TruckIcon = forwardRef<TruckIconHandle, TruckIconProps>(
         {...props}
       >
         <svg
+          className="overflow-visible"
           fill="none"
           height={size}
           stroke="currentColor"
@@ -83,6 +104,26 @@ const TruckIcon = forwardRef<TruckIconHandle, TruckIconProps>(
           width={size}
           xmlns="http://www.w3.org/2000/svg"
         >
+          {[
+            { y: 8, width: 5, x: 0 },
+            { y: 11, width: 7, x: -1 },
+            { y: 14, width: 4, x: 0 },
+          ].map((line, i) => (
+            <motion.line
+              animate={controls}
+              custom={i}
+              initial="normal"
+              key={`speed-${i}`}
+              strokeLinecap="round"
+              strokeWidth="1.5"
+              variants={SPEED_LINE_VARIANTS}
+              x1={line.x}
+              x2={line.x + line.width}
+              y1={line.y}
+              y2={line.y}
+            />
+          ))}
+
           <motion.path
             animate={controls}
             d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
